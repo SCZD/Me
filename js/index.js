4849a1b1c7b343be0019c9fe4f8b805f99c4ea83
sczd <42417471+SCZD@users.noreply.github.com>
2018-12-03 22:24:27 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,7 @@
 var author = "SCZD";
 var date = 2019;
 
-alert("Pre-alpha: this page have some error so that doesnt work fine.");
-
-var respuesta = prompt("Pass");
-alert("Nah only i´m joking.");
-/*
-var confirmacion = confirm("ARE YOU SURE YOU WANT TO CONTINUE THERE IS NO WAY BACK?")
-if (confirmacion == false) {
-    
-    window.open("https://www.google.com/", "", "width=300, height=200") 
-}
-*/
+alert("Pre-alpha:Esta pagina es diseñada en "escritorio");
 
 
 console.log("T4A4B5C883A88AC96DD2886B08FCBB5B5F8CC554686D876E1A4C5CBAB6B351514");
