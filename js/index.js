556b6788f9c3d064b21ccd70909f31865e644547
sczd <42417471+SCZD@users.noreply.github.com>
2018-12-03 22:24:43 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var author = "SCZD";
 var date = 2019;
 
-alert("Pre-alpha:Esta pagina es diseñada en "escritorio");
+alert("Pre-alpha:Esta pagina es diseñada en "escritorio"");
 
 
 console.log("T4A4B5C883A88AC96DD2886B08FCBB5B5F8CC554686D876E1A4C5CBAB6B351514");
